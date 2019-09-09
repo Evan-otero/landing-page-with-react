589346a8d716eed783c3bfed69174dcd65e56074
Evan Otero <Evan.otero19@gmail.com>
2019-09-09 02:23:39 +0000

    jumbortron and navbar

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,24 +2,24 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Navbar } from "./Navbar.js";
+import { Jumbotron } from "./Jumbotron.js";
 
 //create your first component
 export class Home extends React.Component {
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
+			<div className="container-fluid">
+				<div className="row">
+					<div className="col-md-12">
+						<Navbar />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-8">
+						<Jumbotron />
+					</div>
+				</div>
 			</div>
 		);
 	}
